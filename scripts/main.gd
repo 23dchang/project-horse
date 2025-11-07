2a5a8f4ddf48861678445a948dfa74d1514a6eea
Alexander Landess <anlandess@ucdavis.edu>
2025-11-07 13:03:00 -0800

    Added base entity class and players + boss are now dynamically spawned in.

--- scripts/main.gd
+++ scripts/main.gd
@@ -3,11 +3,13 @@ extends Node
 var current_scene: Node
 
 
-func _ready():
+func _ready() -> void:
+	
+	# Game should start on the title screen
 	load_scene("res://scenes/title_screen.tscn")
 
 
-func load_scene(scene_path: String):
+func load_scene(scene_path: String) -> void:
 	
 	# Frees the old	scene when switching
 	if current_scene:
